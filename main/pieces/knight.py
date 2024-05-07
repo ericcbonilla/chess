f23ab8dca3b07fb03424a40985648f9fdc052b0b
Eric Bonilla <ericcbonilla@gmail.com>
2024-05-07 15:21:46 -0700

    Simplify Knight

--- main/pieces/knight.py
+++ main/pieces/knight.py
@@ -21,11 +21,7 @@ class Knight(Piece):
         if (
             new_position not in constants.SQUARES
             or new_position in self.agent.positions
-            or self.king_is_in_check(
-                king=self.king,
-                new_position=new_position,
-            )
+            or self.king_is_in_check(king=self.king, new_position=new_position)
         ):
             return False
-
         return True
