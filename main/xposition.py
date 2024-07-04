3940c723542922f4fc71198a63ad6dcaf42e2f1d
Eric Bonilla <ericcbonilla@gmail.com>
2024-07-03 19:37:53 -0700

    Add caching for XPosition - another big performance improvement

--- main/xposition.py
+++ main/xposition.py
@@ -1,10 +1,25 @@
+from functools import lru_cache
 from operator import neg, pos
 from typing import Callable, Optional
 
 
-class XPosition(str):
+@lru_cache(maxsize=256)
+def operate(operation: Callable, other: int, xpstr: str, wrap: bool) -> "XPosition":
     to_number_map = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8}
     to_letter_map = {1: "a", 2: "b", 3: "c", 4: "d", 5: "e", 6: "f", 7: "g", 8: "h"}
+    new_value = to_number_map[xpstr] + operation(other)
+
+    if new_value not in to_letter_map:
+        if wrap:
+            new_value = ((new_value - operation(8)) % 8) or 8
+        else:
+            return XPosition("", wrap=wrap)
+
+    return XPosition(to_letter_map[new_value], wrap=wrap)
+
+
+class XPosition(str):
+    __slots__ = ("wrap",)
 
     def __new__(cls, value, *args, **kwargs):
         return super().__new__(cls, value)
@@ -12,19 +27,8 @@ class XPosition(str):
     def __init__(self, _, wrap: Optional[bool] = False):
         self.wrap = wrap
 
-    def _operate(self, operation: Callable, other: int) -> "XPosition":
-        new_value = self.to_number_map[self] + operation(other)
-
-        if new_value not in self.to_letter_map:
-            if self.wrap:
-                new_value = ((new_value - operation(8)) % 8) or 8
-            else:
-                return XPosition("", wrap=self.wrap)
-
-        return XPosition(self.to_letter_map[new_value], wrap=self.wrap)
-
     def __sub__(self, other: int):
-        return self._operate(neg, other)
+        return operate(neg, other, str(self), self.wrap)
 
     def __add__(self, other: int):
-        return self._operate(pos, other)
+        return operate(pos, other, str(self), self.wrap)
